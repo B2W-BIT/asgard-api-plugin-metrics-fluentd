0328623e7224a8311f082f20f73ade727c1f11f1
Dalton Barreto <daltonmatos@gmail.com>
2019-01-17 15:54:23 -0200

    Removendo dependencias de teste do setup.py

--- setup.py
+++ setup.py
@@ -5,32 +5,20 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='asgard-api-plugin-metrics-fluentd',
-    version='0.1.1',
-
-    description='Asgard API endpoints to get Fluentd metrics',
+    name="asgard-api-plugin-metrics-fluentd",
+    version="0.1.1",
+    description="Asgard API endpoints to get Fluentd metrics",
     long_description="Plugin para a Asgard API e que fornece métricas do cluster de Fluentd",
-    url='https://github.com/B2W-BIT/asgard-api-plugin-metrics-fluentd',
+    url="https://github.com/B2W-BIT/asgard-api-plugin-metrics-fluentd",
     # Author details
-    author='Dalton Barreto',
-    author_email='daltonmatos@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires = [
-        "asgard-api-sdk>=0.2.0",
-        "python-dateutil==2.7.3",
-        "requests>=2.0.0",
-        "freezegun==0.3.10",
-        "pytz==2018.04",
-    ],
-
+    author="Dalton Barreto",
+    author_email="daltonmatos@gmail.com",
+    license="MIT",
+    classifiers=["Programming Language :: Python :: 3.6"],
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     entry_points={
-        'asgard_api_metrics_mountpoint': [
-            'init = fluentdmetrics.plugin:asgard_api_plugin_init',
-        ],
+        "asgard_api_metrics_mountpoint": [
+            "init = fluentdmetrics.plugin:asgard_api_plugin_init"
+        ]
     },
 )
